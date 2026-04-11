b089e6495547b862f0832ce3c99d1d3c1a1cc457
tiennm99 <tiennm99@outlook.com>
2026-04-11 12:07:18 +0700

    refactor: delete api/, core/, old client/, web/, server/bot/, server/network/tcp.go
    Removed legacy Java API, Go shared core lib, Go CLI client, vanilla-JS web
    client, QQ bot integration, and TCP listener. Server is now WS-only.

--- api/src/main/java/com/isnico/api/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.isnico.api.mapper;
-
-import com.isnico.api.model.po.User;
-import org.nico.ourbatis.mapper.SimpleMapper;
-
-public interface UserMapper extends SimpleMapper<User, String>{
-
-}
